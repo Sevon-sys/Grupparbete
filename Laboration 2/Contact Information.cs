395eda0e8a99d82fbc3d3b792ef4ed0f9243153c
Sevon-sys <73109184+Sevon-sys@users.noreply.github.com>
2020-10-22 15:15:11 +0200

    Add files via upload

---
+++ Laboration 2/Contact Information.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboration_2
+{
+    class ContactInfo
+    {
+        private string _firstName;
+        private string _surName;
+        private string _address;
+        private string _phoneHome;
+        private string _phoneMob;
+
+        public ContactInfo()
+        {
+
+        }
+        public ContactInfo(string firstName, string surName, string address, string phoneHome, string phoneMob)
+        {
+            _firstName = firstName;
+            _surName = surName;
+            _address = address;
+            _phoneHome = phoneHome;
+            _phoneMob = phoneMob;
+        }
+        public string FirstName { get { return _firstName; } set { _firstName = value; } }
+        public string SurName { get { return _surName; } set { _surName = value; } }
+        public string Address { get { return _address; } set { _address = value; } }
+        public string PhoneHome { get { return _phoneHome; } set { _phoneHome = value; } }
+        public string PhoneMob { get { return _phoneMob; } set { _phoneMob = value; } }
+
+        public void Contacts()
+        {
+            Console.Write($"Name: {_surName}, {_firstName} \nAddress: {_address} \nHome number: {_phoneHome} \nPhone Number: {_phoneMob}\n\n");
+        }
+        public void SearchFunc()
+        {
+            //string findFirstName = Console.ReadLine();
+            //if (findFirstName==_firstName)
+            //{
+            //    Console.wr
+            //}
+            //Console.WriteLine(searchFirstName);
+        }
+    }
+}
---
+++ Laboration 2/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboration_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContactInfo contactInfo = new ContactInfo();
+            var contactList = new List<ContactInfo>();
+            contactList.Add(new ContactInfo("Lars", "Carlstedt", "hemma hos sig", "000-00 000", "000-00 00 000"));
+            contactList.Add(new ContactInfo("Oskar", "Vennerlund", "någonstans", "000-00 00 000", "000-00 00 000"));
+            contactList.Add(new ContactInfo("Karin", "Mäki-kala", "på discord", "000-00 00 000", "000-00 00 000"));
+            contactList.Add(new ContactInfo("Simon", "Karvonen", "i ett grupprum", "000-00 00 000", "000-00 00 000"));
+            contactList.Add(new ContactInfo("Andreas", "Hideblad", "skriv in något", "000-00 00 000", "000-00 00 000"));
+            contactList.Add(new ContactInfo("Emil", "Sevon", "Hemma", "000-00 00 000", "073-24 99 538"));
+
+            Console.WriteLine("Welcome to your phonebook.");
+            Console.WriteLine("Choose an option from the list:");
+            Console.WriteLine("add\nremove\nlist\nsearch\nchange\nexit\n");
+
+            string firstName;
+            string surName;
+            string address;
+            string homeNumber;
+            string phoneNumber;
+            
+            while (true)
+            {
+                var choice = Console.ReadLine();
+                choice.ToLower();
+                if (choice == "add")
+                {
+                    Console.Write("Please enter your first name: ");
+                    firstName = Console.ReadLine();
+                    Console.Write("Please enter your surname: ");
+                    surName = Console.ReadLine();
+                    Console.Write("Please enter your address: ");
+                    address = Console.ReadLine();
+                    Console.Write("Please enter your home number: ");
+                    homeNumber = Console.ReadLine();
+                    Console.Write("Please enter your mobile number: ");
+                    phoneNumber = Console.ReadLine();
+                    contactList.Add(new ContactInfo(firstName, surName, address, homeNumber, phoneNumber));
+                    Console.WriteLine("\nChoose a new item in the list: \nadd\nremove\nlist\nsearch\nchange\nexit\n");
+                    continue;
+                }
+                else if (choice == "remove")
+                {
+                    contactList.RemoveAt(Convert.ToInt32(Console.ReadLine()) - 1);
+                    Console.WriteLine("\nChoose a new item in the list: \nadd\nremove\nlist\nsearch\nchange\nexit\n");
+                    continue;
+                }
+                else if (choice == "list")
+                {
+                    foreach (var index in contactList)
+                    {
+                        index.Contacts();
+                    }
+                    Console.WriteLine("\nChoose a new item in the list: \nadd\nremove\nlist\nsearch\nchange\nexit\n");
+                    continue;
+                }
+                else if (choice == "search")
+                {
+                    //string findFirstName = Console.ReadLine();
+                    //contactList.Find(x => x.FirstName.Contains(findFirstName));
+
+                    Console.WriteLine("\nChoose a new item in the list: \nadd\nremove\nlist\nsearch\nchange\nexit\n");
+                    continue;
+                }
+                else if (choice == "change")
+                {
+                    Console.WriteLine("\nWhich contact would you like to change?\n");
+                    foreach (var item in contactList)
+                    {
+                        item.Contacts();
+                    }
+                    int user_Index = Convert.ToInt32(Console.ReadLine());
+                    contactList.ElementAt(user_Index - 1);
+
+                    Console.WriteLine("What do you want to change? \n'first name', 'surname', 'address', 'home number', 'mobile number' or 'exit' to save changes");
+                    var choice2 = Console.ReadLine();
+                    choice2.ToLower();
+                    if (choice2 == "first name" || choice2 == "firstname")
+                    {
+                        var newFirstName = Console.ReadLine();
+                        //ContactInfo. = newFirstName;
+                    }
+                    else if (choice2 == "surname")
+                    {
+                        var newSurName = Console.ReadLine();
+                        //ContactInfo. = newSurName;
+                    }
+                    else if (choice2 == "address")
+                    {
+                        var newAddress = Console.ReadLine();
+                        //ContactInfo. = newAddress;
+                    }
+                    else if (choice2 == "home number" || choice2 == "homenumber")
+                    {
+                        var newHomeNumber = Console.ReadLine();
+                        //ContactInfo. = newHomeNumber;
+                    }
+                    else if (choice2 == "mobile number" || choice2 == "mobilenumber")
+                    {
+                        var newPhoneNumber = Console.ReadLine();
+                        //ContactInfo.= newPhoneNumber;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You did not enter a valid option: choose an item in the list: \nadd\nremove\nlist\nsearch\nchange\nexit\n");
+                    }
+                    Console.WriteLine("\nChoose a new item in the list: \nadd\nremove\nlist\nsearch\nchange\nexit\n");
+                }
+                else if (choice == "exit")
+                {
+                    Console.WriteLine("\nChoose a new item in the list: \nadd\nremove\nlist\nsearch\nchange\nexit\n");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid option, choose a new item in the list: \nadd\nremove\nlist\nsearch\nchange\nexit\n");
+                    continue;
+                }
+            }
+            //contactInfo.Contacts();
+        }
+    }
+}
