143fe59f4eb66e5a12d2f72314f207e4c7795c72
Sevon-sys <73109184+Sevon-sys@users.noreply.github.com>
2020-10-23 12:07:45 +0200

    Add files via upload

--- Laboration 2/Contact Information.cs
+++ Laboration 2/Contact Information.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Laboration_2
 {
@@ -14,10 +10,6 @@ namespace Laboration_2
         private string _phoneHome;
         private string _phoneMob;
 
-        public ContactInfo()
-        {
-
-        }
         public ContactInfo(string firstName, string surName, string address, string phoneHome, string phoneMob)
         {
             _firstName = firstName;
@@ -26,6 +18,7 @@ namespace Laboration_2
             _phoneHome = phoneHome;
             _phoneMob = phoneMob;
         }
+
         public string FirstName { get { return _firstName; } set { _firstName = value; } }
         public string SurName { get { return _surName; } set { _surName = value; } }
         public string Address { get { return _address; } set { _address = value; } }
@@ -36,14 +29,9 @@ namespace Laboration_2
         {
             Console.Write($"Name: {_surName}, {_firstName} \nAddress: {_address} \nHome number: {_phoneHome} \nPhone Number: {_phoneMob}\n\n");
         }
+
         public void SearchFunc()
         {
-            //string findFirstName = Console.ReadLine();
-            //if (findFirstName==_firstName)
-            //{
-            //    Console.wr
-            //}
-            //Console.WriteLine(searchFirstName);
         }
     }
 }
